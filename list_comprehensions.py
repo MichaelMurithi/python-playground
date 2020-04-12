e434007a0e6d283deb65d408a294592fe73eec0b
Myke <mykemitrichev@gmail.com>
2020-04-12 14:28:50 +0300

    Added use of inbuilt list operations

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -18,3 +18,16 @@ print(even_squares)
 # To convert the numbers into a comma separated string
 string_squares =','.join([str(even_square) for even_square in even_squares])
 print(string_squares)
+
+#sum() funtion is inbuilt to take a list and return the sum of sll elements in the list
+#min() funtion takes a list of integers and returns the minimum value
+# max() takes a list of integers and returns the maximum value
+
+#function to take a coma separated string of numbers and return its sum
+num_string = "4, 5, 10, 70, 12"
+def sum_of_string(num_string):
+    nums_str_list = num_string.split(", ")
+    nums = [int(num) for num in nums_str_list]
+    return sum(nums)
+
+print(sum_of_string(num_string))
