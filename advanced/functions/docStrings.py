0c9821bceb650619012ec9afcea424b46859dee6
Myke <mykemitrichev@gmail.com>
2020-07-04 09:50:18 +0300

    Practiced using doc strings

---
+++ advanced/functions/docStrings.py
@@ -0,0 +1,12 @@
+# Docstrings enable documentation of python funtions 
+
+# ** The following rules are followed when writting docstrings **
+'''
+* They should be enclosed in tripple quotes
+* First line should be summary sentence of functionality
+* modules: important classes, functions and exceptions should be listed
+* classes: list important methods along with important information like enums or static member funtions
+* Functions: List parameters and explain each one per line
+    - If the function returns a value, explain it in the docstring, else omit it.
+    - If the function raises an exception, mention it
+'''
