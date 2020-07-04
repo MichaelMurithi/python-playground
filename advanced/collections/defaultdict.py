973b5d2ca8176c4efd6166a2b0ea9a4a8b927d18
Myke <mykemitrichev@gmail.com>
2020-07-04 10:59:52 +0300

    Practiced use of defaultdict

---
+++ advanced/collections/defaultdict.py
@@ -0,0 +1,28 @@
+# defaultdict takes a factory method and returns a default value for a key which is not available in a list
+# Demonstrate the usage of defaultdict objects
+
+from collections import defaultdict
+
+def getDefaultValue():
+    return 0
+
+def main():
+    # define a list of items that we want to count
+    fruits = ['apple', 'pear', 'orange', 'banana',
+              'apple', 'grape', 'banana', 'banana']
+
+    # use a dictionary to count each element
+    fruitCounter = defaultdict(getDefaultValue) #will use inbuilt integer function to assign default values 
+        
+    
+    # Count the elements in the list
+    for fruit in fruits:
+        fruitCounter[fruit] += 1
+
+    # print the result
+    for (k, v) in fruitCounter.items():
+        print(k + ": " + str(v))
+
+
+if __name__ == "__main__":
+    main()
