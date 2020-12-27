e36ab7513cbfa3aa7a0ff5b4efeb834cfa772203
mykemk <mykemitrichev@gmail.com>
2020-12-27 07:40:00 +0100

    Practised Multilevel Inheritance

---
+++ oop/MultiLevelInheritance.py
@@ -0,0 +1,22 @@
+class GrandParents:
+    
+    def PropertyLand(self):
+        print("Property Land for farming given by Grand parents ****")
+
+    
+class Parents(GrandParents):
+
+    def PropertyHome(self):
+        print("Property Home constructed by Parents ***")
+
+
+class Child(Parents):
+
+    def PropertyVehicle(self):
+        print("Property Porsche purchased by Child **")
+
+
+first_born = Child()
+first_born.PropertyLand()
+first_born.PropertyHome()
+first_born.PropertyVehicle()
